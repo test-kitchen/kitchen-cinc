a6c338716390e979d90b9faf00cbd147e94a09c9
Lance Albertson <lance@osuosl.org>
2026-04-19 16:32:39 -0700

    test: add dokken config and update integration tests
    - Add kitchen.dokken.yml using cincproject/cinc Docker image
    - Update test cookbook to create a directory (matches upstream pattern)
    - Add directory existence check to integration spec
    - Update kitchen.yml platforms to ubuntu-24.04 and almalinux-9
    - Enable deprecations_as_errors in kitchen.yml
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test/cookbooks/test_cookbook/recipes/default.rb
+++ test/cookbooks/test_cookbook/recipes/default.rb
@@ -1,1 +1,3 @@
-log "Hello from Cinc Client!"
+dir = windows? ? "c:\\tk_test_directory" : "/tmp/tk_test_directory"
+
+directory dir
--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,3 +1,9 @@
+dir = os.windows? ? "c:\\tk_test_directory" : "/tmp/tk_test_directory"
+
+describe directory(dir) do
+  it { should exist }
+end
+
 describe command("cinc-client -v") do
   its("stdout") { should match(/Cinc Client/) }
 end
